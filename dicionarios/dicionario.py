b66ee61739f0a99065caeea145bdb93c2343bd62
LucasHavranek <81269366+LucasHavranek@users.noreply.github.com>
2022-01-31 18:33:50 -0300

    dicionario

---
+++ dicionarios/dicionario.py
@@ -0,0 +1,10 @@
+usuarios = {}
+
+usuarios = {
+    "chaves": ["Chaves do 8", "20/12/2021", "REC01"],
+    "quico": ["Quico", "20/12/2021", "ENF02"]
+}
+
+usuarios["Florinda"] = ["Dona Florinda", "21/12/2021", "AUD01"]
+print(usuarios)
+print(usuarios.get("quico"))
