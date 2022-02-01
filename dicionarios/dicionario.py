e0130eacd4bf4d1034308402e95a050e2c7cdae0
LucasHavranek <81269366+LucasHavranek@users.noreply.github.com>
2022-01-31 23:31:16 -0300

    import corrigido

--- dicionarios/dicionario.py
+++ dicionarios/dicionario.py
@@ -1,12 +1,8 @@
-from dicionarios.funcoes import *
+from funcoes import *
 
 usuarios = {}
-
-usuarios = {
-    "chaves": ["Chaves do 8", "20/12/2021", "REC01"],
-    "quico": ["Quico", "20/12/2021", "ENF02"]
-}
-
-usuarios["Florinda"] = ["Dona Florinda", "21/12/2021", "AUD01"]
-print(usuarios)
-print(usuarios.get("quico"))
+opcao = perguntar()
+while opcao == "I" or opcao == "P" or opcao == "E" or opcao == "L":
+    if opcao == "I":
+        inserir(usuarios)
+    opcao = perguntar()
--- dicionarios/funcoes.py
+++ dicionarios/funcoes.py
@@ -1,11 +1,13 @@
 def perguntar():
     return input("O que deseja realizar?\n" +
-    "<I> - Para inserir um usuário\n" +
-    "<P> - Para inserir um usuário\n" +
-    "<E> - Para inserir um usuário\n" +
-    "<L> - Para inserir um usuário: ").upper()
+                 "<I> - Para inserir um usuário\n" +
+                 "<P> - Para pesquisar um usuário\n" +
+                 "<E> - Para editar um usuário\n" +
+                 "<L> - Para listar um usuário: ").upper()
+
 
 def inserir(dicionario):
     dicionario[input("Digite o login: ").upper()] = [input("Digite o nome: ").upper(),
-                                                    input("Digite a última data de acesso: "),
-                                                    input("Qual a última estação acessada: ").upper()]
+                                                     input(
+                                                         "Digite a última data de acesso: "),
+                                                     input("Qual a última estação acessada: ").upper()]
