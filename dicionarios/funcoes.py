86fa53d838736c927e701ce439d93005b610d1ab
LucasHavranek <81269366+LucasHavranek@users.noreply.github.com>
2022-02-01 21:45:50 -0300

    salvando dados em um arquivo txt

--- dicionarios/funcoes.py
+++ dicionarios/funcoes.py
@@ -1,3 +1,10 @@
+import os.path
+
+save_path = './dicionarios/'
+file_name = 'bd.txt'
+complete_directory = os.path.join(save_path, file_name)
+
+
 def perguntar():
     return input("O que deseja realizar?\n" +
                  "<I> - Para inserir um usuário\n" +
@@ -8,6 +15,12 @@ def perguntar():
 
 def inserir(dicionario):
     dicionario[input("Digite o login: ").upper()] = [input("Digite o nome: ").upper(),
-                                                     input(
-                                                         "Digite a última data de acesso: "),
+                                                     input("Digite a última data de acesso: "),
                                                      input("Qual a última estação acessada: ").upper()]
+    salvar(dicionario)
+
+
+def salvar(dicionario):
+    with open(complete_directory, "a") as arquivo:
+        for chave, valor in dicionario.items():
+            arquivo.write(chave + ":" + str(valor))
