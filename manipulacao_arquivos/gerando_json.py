9e294782ea22dfd25928c3be04c2278d706e531b
LucasHavranek <81269366+LucasHavranek@users.noreply.github.com>
2022-02-01 22:10:29 -0300

    versao final json

--- manipulacao_arquivos/gerando_json.py
+++ manipulacao_arquivos/gerando_json.py
@@ -1,8 +1,13 @@
 import json
+import os.path
+
+save_path = './manipulacao_arquivos/'
+file_name = 'bd.json'
+complete_directory = os.path.join(save_path, file_name)
 
 dicionario = {
     "Fulano": ["Fulano da Silva", "14/10/2021", "AT09"]
 }
 
-with open ("bd.json", "a") as json_file:
+with open (complete_directory, "a") as json_file:
     json.dump(dicionario, json_file)
